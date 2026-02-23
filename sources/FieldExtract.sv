183d55a748217dbf0b8595395a6951e965c90200
Jagini Mohit <144369341+Mohit14036@users.noreply.github.com>
2026-02-23 10:12:10 +0000

    changed fieldextract

--- sources/FieldExtract.sv
+++ sources/FieldExtract.sv
@@ -3,7 +3,6 @@
 module FieldExtract (
     input  logic [31:0] V,
     input  logic [1:0]  pres,
-    input  logic        vec,
     output logic [15:0] exp,
     output logic [31:0] fraction
 );
@@ -12,50 +11,32 @@ always_comb begin
     exp      = '0;
     fraction = '0;
 
-    unique case ({vec, pres})
+    unique case (pres)
 
-    3'b100: begin
+    2'b00: begin
         exp = { V[30:27], V[22:19], V[14:11], V[6:3] };
+
         fraction = {
-            5'd0, V[26:24],
-            5'd0, V[18:16],
-            5'd0, V[10:8],
-            5'd0, V[2:0]
+            4'b0001, V[26:24],
+            4'b0001, V[18:16],
+            4'b0001, V[10:8],
+            4'b0001, V[2:0]
         };
     end
 
-    3'b101: begin
+    2'b01: begin
         exp = { 6'd0, V[30:26], V[14:10] };
-        fraction = {
-            6'd0, V[25:16],
-            6'd0, V[9:0]
-        };
-    end
 
-    3'b110: begin
-        exp = { 8'd0, V[30:23] };
-        fraction = { 9'd0, V[22:0] };
-    end
-
-    3'b000: begin
-        exp = { 12'd0, V[30:27] };
         fraction = {
-            24'd0,
-            5'd0, V[26:24]
-        };
-    end
-
-    3'b001: begin
-        exp = { 11'd0, V[30:26] };
-        fraction = {
-            16'd0,
-            6'd0, V[25:16]
+            5'b00001, V[25:16],
+            5'b00001, V[9:0]
         };
     end
 
     default: begin
         exp = { 8'd0, V[30:23] };
-        fraction = { 9'd0, V[22:0] };
+
+        fraction = { 1'b1, V[22:0] };
     end
 
     endcase
