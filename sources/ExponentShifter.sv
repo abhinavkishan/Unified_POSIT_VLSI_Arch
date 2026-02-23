71d1ff0c0cc07b4884b6877096a91df615d947fc
Jagini Mohit <144369341+Mohit14036@users.noreply.github.com>
2026-02-23 10:04:54 +0000

    changed

--- sources/ExponentShifter.sv
+++ sources/ExponentShifter.sv
@@ -9,6 +9,7 @@ module ExponentShifter (
 );
 
 logic [31:0] mask;
+logic [31:0] frac_mask;
 
 always_comb begin
     fraction = '0;
@@ -21,18 +22,30 @@ always_comb begin
     2'b00: begin
         for (int i = 0; i < 4; i++) begin
             logic [7:0] lane;
+            logic [7:0] frac_bits;
+            int frac_width;
+
             lane = vin[i*8 +: 8];
 
             exp[i*8 +: 8] =
                 (lane >> (6 - es + 1)) & mask;
 
+            frac_width = 7 - es;
+
+            frac_mask  = (32'h1 << frac_width) - 1;
+            frac_bits  = lane & frac_mask;
+
             fraction[i*8 +: 8] =
-                lane & ((1 << (6 - es + 1)) - 1);
+                (1 << frac_width) | frac_bits;
         end
     end
 
     2'b01: begin
         logic [15:0] lane0, lane1;
+        int frac_width;
+
+        frac_width = 15 - es;
+        frac_mask  = (32'h1 << frac_width) - 1;
 
         lane0 = vin[15:0];
         lane1 = vin[31:16];
@@ -40,19 +53,25 @@ always_comb begin
         exp[15:0] =
             (lane0 >> (14 - es + 1)) & mask;
         fraction[15:0] =
-            lane0 & ((1 << (14 - es + 1)) - 1);
+            (1 << frac_width) | (lane0 & frac_mask);
 
         exp[31:16] =
             (lane1 >> (14 - es + 1)) & mask;
         fraction[31:16] =
-            lane1 & ((1 << (14 - es + 1)) - 1);
+            (1 << frac_width) | (lane1 & frac_mask);
     end
 
     2'b10: begin
+        int frac_width;
+
+        frac_width = 31 - es;
+        frac_mask  = (32'h1 << frac_width) - 1;
+
         exp =
             (vin >> (30 - es + 1)) & mask;
+
         fraction =
-            vin & ((1 << (30 - es + 1)) - 1);
+            (1 << frac_width) | (vin & frac_mask);
     end
 
     endcase
