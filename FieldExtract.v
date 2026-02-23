c5755ace6c26e8413948bc6898ba869123ab5ae0
Jagini Mohit <144369341+Mohit14036@users.noreply.github.com>
2026-02-23 05:50:12 +0000

    added file

--- FieldExtract.v
+++ FieldExtract.v
@@ -5,45 +5,67 @@ module FieldExtract(
     input  [1:0]  pres,   // 00=8bit, 01=16bit, 10=32bit
     input         vec,
     output reg [15:0] exp,
-    output reg [22:0] fraction
+    output reg [31:0] fraction   
 );
 
 always @(*) begin
     exp      = 16'd0;
-    fraction = 23'd0;
+    fraction = 32'd0;
 
     case ({vec, pres})
 
     3'b100: begin
-        exp      = { V[30:27], V[22:19], V[14:11], V[6:3] };
-        fraction = { 11'd0,
-                     V[26:24], V[18:16],
-                     V[10:8],  V[2:0] };
+        exp = { V[30:27], V[22:19], V[14:11], V[6:3] };
+
+        fraction = {
+            5'd0, V[26:24],   
+            5'd0, V[18:16],   
+            5'd0, V[10:8],    
+            5'd0, V[2:0]      
+        };
     end
 
     3'b101: begin
-        exp      = { 6'd0, V[30:26], V[14:10] };
-        fraction = { 3'd0, V[25:16], V[9:0] };
+        exp = { 6'd0, V[30:26], V[14:10] };
+
+        fraction = {
+            6'd0, V[25:16],  
+            6'd0, V[9:0]      
+        };
     end
 
     3'b110: begin
-        exp      = { 8'd0, V[30:23] };
-        fraction = V[22:0];
+        exp = { 8'd0, V[30:23] };
+
+        fraction = {
+            9'd0, V[22:0]
+        };
     end
 
     3'b000: begin
-        exp      = { 12'd0, V[30:27] };
-        fraction = { 20'd0, V[26:24] };
+        exp = { 12'd0, V[30:27] };
+
+        fraction = {
+            24'd0,
+            5'd0, V[26:24]
+        };
     end
 
     3'b001: begin
-        exp      = { 11'd0, V[30:26] };
-        fraction = { 13'd0, V[25:16] };
+        exp = { 11'd0, V[30:26] };
+
+        fraction = {
+            16'd0,
+            6'd0, V[25:16]
+        };
     end
 
     default: begin
-        exp      = { 8'd0, V[30:23] };
-        fraction = V[22:0];
+        exp = { 8'd0, V[30:23] };
+
+        fraction = {
+            9'd0, V[22:0]
+        };
     end
 
     endcase
