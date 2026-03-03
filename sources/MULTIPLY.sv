0d94fb5d756e698f68ccca7f1a75a7e0d4670072
Jagini Mohit <144369341+Mohit14036@users.noreply.github.com>
2026-03-03 11:17:53 +0000

    Done multiply block

--- sources/MULTIPLY.sv
+++ sources/MULTIPLY.sv
@@ -1,15 +1,17 @@
 `timescale 1ns/1ps
 
 module MULTIPLY(
-input logic [3:0] sa,sb,sc,
-input logic [31:0] sfa,sfb,sfc,
-input logic [31:0] fa,fb,fc,
+input logic [3:0] sa,sb,
+input logic [31:0] sfa,sfb,
+input logic [31:0] fa,fb,
 output logic [31:0] sfp,
-output logic [:0] fp,
+output logic [63:0] fp,
 output logic [3:0] sp,
-
+input logic [5:0] ctrlr
 );
 
-
+mult32_fraction_vector rsab(fa,fb,ctrlr[5:4],fp);
+assign sp=sa^sb;
+vector_sf_adder vsa(.a(sfa),.b(sfb),.p_m(ctrlr[5:4]),.sum(sfp));
 
 endmodule
--- sources/radix_4_multiply.sv
+++ sources/radix_4_multiply.sv
@@ -1,87 +1,143 @@
-`timescale 1ns / 1ps
-
-module vector_multiplier #(
-    parameter W = 32
-)(
+module mult32_fraction_vector (
     input  logic [31:0] A,
     input  logic [31:0] B,
-    input  logic [1:0]  precision_mode,   // 00=8bit, 01=16bit, 10=32bit
+    input  logic [1:0]  mode,   // 00=4x8, 01=2x16, 10=1x32
     output logic [63:0] P
 );
 
-    logic [7:0] A_lane [3:0];
-    logic [7:0] B_lane [3:0];
+    logic [7:0] A_blk [3:0];
+    logic [7:0] B_blk [3:0];
 
-    logic [15:0] pp [3:0][3:0];
-    logic [63:0] shifted_pp [3:0][3:0];
+    assign {A_blk[3],A_blk[2],A_blk[1],A_blk[0]} = A;
+    assign {B_blk[3],B_blk[2],B_blk[1],B_blk[0]} = B;
 
-    
-    assign {A_lane[3], A_lane[2], A_lane[1], A_lane[0]} = A;
-    assign {B_lane[3], B_lane[2], B_lane[1], B_lane[0]} = B;
+    logic [15:0] P_blk [3:0][3:0];
 
-   
+    genvar i,j;
+    generate
+        for(i=0;i<4;i++) begin
+            for(j=0;j<4;j++) begin
+                booth8_radix4_unsigned u_mul (
+                    .a(A_blk[i]),
+                    .b(B_blk[j]),
+                    .p(P_blk[i][j])
+                );
+            end
+        end
+    endgenerate
+
+    logic [63:0] PP [15:0];
+
+    generate
+        for(i=0;i<4;i++) begin
+            for(j=0;j<4;j++) begin
+                assign PP[4*i+j] =
+                    {48'd0, P_blk[i][j]} << (8*(i+j));
+            end
+        end
+    endgenerate
+
+    logic [63:0] s1[4:0], c1[4:0];
+    logic [63:0] r1;
+
+    csa3 s10(PP[0], PP[1], PP[2],  s1[0], c1[0]);
+    csa3 s11(PP[3], PP[4], PP[5],  s1[1], c1[1]);
+    csa3 s12(PP[6], PP[7], PP[8],  s1[2], c1[2]);
+    csa3 s13(PP[9], PP[10],PP[11], s1[3], c1[3]);
+    csa3 s14(PP[12],PP[13],PP[14], s1[4], c1[4]);
+
+    assign r1 = PP[15];
+
+    logic [63:0] s2[2:0], c2[2:0];
+    logic [63:0] r2[1:0];
+
+    csa3 s20(s1[0], c1[0], s1[1], s2[0], c2[0]);
+    csa3 s21(c1[1], s1[2], c1[2], s2[1], c2[1]);
+    csa3 s22(s1[3], c1[3], s1[4], s2[2], c2[2]);
+
+    assign r2[0] = c1[4];
+    assign r2[1] = r1;
+
+    logic [63:0] s3[1:0], c3[1:0];
+    logic [63:0] r3[1:0];
+
+    csa3 s30(s2[0], c2[0], s2[1], s3[0], c3[0]);
+    csa3 s31(c2[1], s2[2], c2[2], s3[1], c3[1]);
+
+    assign r3[0] = r2[0];
+    assign r3[1] = r2[1];
+
+    logic [63:0] s4, c4;
+    logic [63:0] r4[2:0];
+
+    csa3 s40(s3[0], c3[0], s3[1], s4, c4);
+
+    assign r4[0] = c3[1];
+    assign r4[1] = r3[0];
+    assign r4[2] = r3[1];
+
+    logic [63:0] s5, c5;
+    logic [63:0] r5;
+
+    csa3 s50(s4, c4, r4[0], s5, c5);
+
+    assign r5 = r4[1] + r4[2]; 
+
+    logic [63:0] full32;
+
+    assign full32 = s5 + c5 + r5;
+
+    logic [63:0] simd16;
+
     always_comb begin
+        logic [31:0] low16, high16;
 
-        for (int i = 0; i < 4; i++) begin
-            for (int j = 0; j < 4; j++) begin
-                pp[i][j]        = 16'd0;
-                shifted_pp[i][j]= 64'd0;
-            end
-        end
+        low16 =
+            P_blk[0][0] +
+            (P_blk[0][1] << 8) +
+            (P_blk[1][0] << 8) +
+            (P_blk[1][1] << 16);
 
-        case (precision_mode)
+        high16 =
+            P_blk[2][2] +
+            (P_blk[2][3] << 8) +
+            (P_blk[3][2] << 8) +
+            (P_blk[3][3] << 16);
 
-       
-        2'b10: begin
-            for (int i = 0; i < 4; i++) begin
-                for (int j = 0; j < 4; j++) begin
-                    pp[i][j] = A_lane[i] * B_lane[j];
-                end
-            end
-        end
-
-       
-        2'b01: begin
-            for (int i = 0; i < 2; i++) begin
-                for (int j = 0; j < 2; j++) begin
-                    pp[i][j] = A_lane[i] * B_lane[j];
-                end
-            end
-
-            for (int i = 2; i < 4; i++) begin
-                for (int j = 2; j < 4; j++) begin
-                    pp[i][j] = A_lane[i] * B_lane[j];
-                end
-            end
-        end
-
-        
-        2'b00: begin
-            for (int i = 0; i < 4; i++) begin
-                pp[i][i] = A_lane[i] * B_lane[i];
-            end
-        end
-
-        endcase
-
-        
-        for (int i = 0; i < 4; i++) begin
-            for (int j = 0; j < 4; j++) begin
-                shifted_pp[i][j] = 
-                    64'(pp[i][j]) << (8*(i+j));
-            end
-        end
-
+        simd16 = {high16, low16};
     end
 
-    always_comb begin
-        P = 64'd0;
+    logic [63:0] simd8;
 
-        for (int i = 0; i < 4; i++) begin
-            for (int j = 0; j < 4; j++) begin
-                P = P + shifted_pp[i][j];
-            end
-        end
+    always_comb begin
+        simd8 = {
+            P_blk[3][3],
+            P_blk[2][2],
+            P_blk[1][1],
+            P_blk[0][0]
+        };
+    end
+
+ 
+    always_comb begin
+        case(mode)
+            2'b10: P = full32;
+            2'b01: P = simd16;
+            2'b00: P = simd8;
+            default: P = 64'd0;
+        endcase
     end
 
 endmodule
+module csa3 (
+        input  logic [63:0] x,
+        input  logic [63:0] y,
+        input  logic [63:0] z,
+        output logic [63:0] s,
+        output logic [63:0] c
+    );
+        begin
+            assign s = x ^ y ^ z;
+            assign c = ((x & y) | (y & z) | (x & z)) << 1;
+        end
+    endmodule
